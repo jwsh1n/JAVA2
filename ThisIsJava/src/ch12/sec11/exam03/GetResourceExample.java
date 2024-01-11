bb78566d1baab7f8715d19aa2013cf66d0f09c81
jwshin <singee98@naver.com>
2024-01-11 09:47:55 +0900

    24.01.11

--- ThisIsJava/src/ch12/sec11/exam03/GetResourceExample.java
+++ ThisIsJava/src/ch12/sec11/exam03/GetResourceExample.java
@@ -8,8 +8,8 @@ public class GetResourceExample {
 		String photo1Path = clazz.getResource("photo1.jpg").getPath();
 		String photo2Path = clazz.getResource("images/photo2.jpg").getPath();
 
-		System.out.println("photo1Path");
-		System.out.println("photo2Path");
+		System.out.println(photo1Path);
+		System.out.println(photo2Path);
 	}
 
 }
