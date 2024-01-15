1cf623bfd0ed7688074e1c782cf315e74669ee53
jwshin <singee98@naver.com>
2024-01-15 19:57:27 +0900

    24.01.15

---
+++ ThisIsJava/src/ch15/sec03/exam01/HashSetExample.java
@@ -0,0 +1,23 @@
+package ch15.sec03.exam01;
+
+import java.util.*;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		
+		Set<String> set = new HashSet<String>();
+		
+		set.add("Java");
+		set.add("JDBC");
+		set.add("JSP");
+		set.add("Java");
+		set.add("Spring");
+		
+		int size = set.size();
+		System.out.println("총 객체수 : "+size);
+
+
+	}
+
+}
