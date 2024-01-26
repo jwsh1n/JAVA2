4f77d9ee7d93e37bf36bab6330bfc784d2459ad7
jwshin <singee98@naver.com>
2024-01-26 14:49:54 +0900

    2024.01.26

--- ThisIsJava/src/sec03/exam02_jwindow/JWindowExample.java
+++ ThisIsJava/src/sec03/exam02_jwindow/JWindowExample.java
@@ -1,3 +1,4 @@
+
 package sec03.exam02_jwindow;
 
 import java.awt.BorderLayout;
@@ -10,11 +11,35 @@ import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 
-public class JWindowExample {
+public class JWindowExample extends JWindow {
+	public JWindowExample() {
+		this.setSize(595, 350);
+
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Point centerPoint = ge.getCenterPoint();
+		int leftTopX = centerPoint.x - this.getWidth() / 2;
+		int leftTopY = centerPoint.y - this.getHeight() / 2;
+		this.setLocation(leftTopX, leftTopY);
+
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon(getClass().getResource("game.png")));
+		getContentPane().add(label, BorderLayout.CENTER);
+
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+			}
+		});
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JWindowExample jWindow = new JWindowExample();
+				jWindow.setVisible(true);
+			}
+		});
 	}
 
 }
---
+++ ThisIsJava/src/sec03/exam04_jtabbedpane/JTabbedPaneExample.java
@@ -0,0 +1,63 @@
+package sec03.exam04_jtabbedpane;
+
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+
+public class JTabbedPaneExample extends JFrame {
+	private JTabbedPane jTabbdePane;
+	private JPanel tab1Panel;
+	private JPanel tab2Panel;
+
+	public JTabbedPaneExample() {
+		this.setTitle("JTabbedPaneExample");
+		this.setSize(300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().add(getJTabbedPane(), BorderLayout.CENTER);
+	}
+
+	private JTabbedPane getJTabbedPane() {
+		if (jTabbdePane == null) {
+			jTabbdePane = new JTabbedPane();
+			jTabbdePane.setTabPlacement(JTabbedPane.LEFT);
+			jTabbdePane.addTab("탭1", getTab1Panel());
+			jTabbdePane.addTab("탭2", getTab2Panel());
+		}
+		return jTabbdePane;
+	}
+
+	private JPanel getTab1Panel() {
+		if (tab1Panel == null) {
+			tab1Panel = new JPanel();
+			JLabel jLabel = new JLabel();
+			jLabel.setIcon(new ImageIcon(getClass().getResource("duke1.gif")));
+			tab1Panel.add(jLabel);
+		}
+		return tab1Panel;
+	}
+
+	// Tab2에 추가될 JPanel 생성
+	private JPanel getTab2Panel() {
+		if (tab2Panel == null) {
+			tab2Panel = new JPanel();
+			JLabel jLabel = new JLabel();
+			jLabel.setIcon(new ImageIcon(getClass().getResource("duke2.gif")));
+			tab2Panel.add(jLabel);
+		}
+		return tab2Panel;
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JTabbedPaneExample jFrame = new JTabbedPaneExample();
+				jFrame.setVisible(true);
+			}
+		});
+	}
+}
